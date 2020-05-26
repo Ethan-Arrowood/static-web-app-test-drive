2fd787db2c06df07a91a7e5fc729befcdf178768
Ethan Arrowood <ethan.arrowood@gmail.com>
2020-05-26 10:49:32 -0400

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>Created by Ethan Arrowood</p>
+    </div>
+  )
 }
 
 export default App;
