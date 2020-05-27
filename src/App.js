ac3e380faee010e898af76e02b37fa9508b0555d
Ethan Arrowood <ethan.arrowood@gmail.com>
2020-05-27 09:32:49 -0400

    add azure functions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
 function App() {
+  const [ message, setMessage ] = React.useState()
+
+  React.useState(() => {
+    fetch('/api/message?name=Ethan')
+      .then(res => res.json())
+      .then(res => setMessage(res.text))
+  })
   return (
     <div>
-      <h1>Hello, World!</h1>
-      <p>Created by Ethan Arrowood</p>
+      <h1>{ message || 'Hello, World!'}</h1>
+      <p>Static Web App Test Drive</p>
     </div>
   )
 }
